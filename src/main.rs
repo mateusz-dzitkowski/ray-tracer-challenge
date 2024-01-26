426e72abb2793137016c0dcb5d9ca906b7469b61
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2024-01-26 20:11:10 +0100

    playing with the projectile concept

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use nalgebra::Vector3;
+
+
+type Vector = Vector3<f32>;
+
+struct Env {
+    gravity: Vector,
+    wind: Vector,
+}
+
+struct Projectile {
+    position: Vector,
+    velocity: Vector,
+}
+
+fn tick(projectile: Projectile, env: &Env) -> Projectile {
+    Projectile {
+        position: projectile.position + projectile.velocity,
+        velocity: projectile.velocity + env.gravity + env.wind,
+    }
+}
+
 fn main() {
-    println!("Hi, Mom!");
+    let mut p = Projectile {
+        position: Vector::new(0., 1., 0.),
+        velocity: Vector::new(1., 1., 0.).normalize(),
+    };
+    let e = Env {
+        gravity: Vector::new(0., -0.1, 0.),
+        wind: Vector::new(-0.01, 0., 0.),
+    };
+
+    while p.position.y >= 0. {
+        println!("{}, {}, {}", p.position.x, p.position.y, p.position.z);
+        p = tick(p, &e);
+    }
 }
