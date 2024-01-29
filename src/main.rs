a1268031c9b38b3adb3ebfafe414d35d0cda8259
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2024-01-29 21:49:32 +0100

    make a clock

--- src/main.rs
+++ src/main.rs
@@ -3,46 +3,20 @@ mod colour;
 mod types;
 
 use canvas::Canvas;
+use num_traits::FloatConst;
 use types::*;
 
-#[derive(Clone, Copy)]
-struct Projectile {
-    position: Point,
-    velocity: Vector,
-}
-
-#[derive(Clone, Copy)]
-struct Environment {
-    gravity: Vector,
-    wind: Vector,
-}
-
-fn tick<const W: usize, const H: usize>(p: &mut Projectile, c: &mut Canvas<W, H>, e: Environment) {
-    p.position = p.position + p.velocity;
-    p.velocity = p.velocity + e.gravity + e.wind;
-
-    let w_index = p.position.x as usize;
-    let h_index = p.position.y as usize;
-    let colour = colour::cyan();
-    c.set(w_index, h_index, colour);
-}
-
 fn main() {
-    let mut p = Projectile {
-        position: Point::new(0., 1., 0.),
-        velocity: Vector::new(1., 1.8, 0.).normalize() * 11.25,
-    };
-
-    let e = Environment {
-        gravity: Vector::new(0., -0.2, 0.),
-        wind: Vector::new(-0.01, 0., 0.),
-    };
-
-    let mut c: Canvas<900, 550> = Canvas::default();
-
-    while p.position.y >= 0. {
-        tick(&mut p, &mut c, e);
+    let mut canvas: Canvas<51, 51> = Canvas::default();
+    let c = colour::red();
+    for n in 0..12 {
+        let p = Point::new(20., 0., 0.);
+        let rotation = Rotation::new(Vector::new(0., 0., f32::PI() * n as f32 / 6.));
+        let translation = Translation::new(25., 25., 0.);
+        let w = translation * rotation * p;
+        let i = w.x.round() as usize;
+        let j = w.y.round() as usize;
+        canvas.set(i, j, c);
     }
-
-    c.to_ppm_file("canvas.ppm");
+    canvas.to_ppm_file("clock.ppm");
 }
